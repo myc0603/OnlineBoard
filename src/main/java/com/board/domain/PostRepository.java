5700e7cc3ca2e405af140783ffabb575ca479679
Yechan Moon <moonyechan97@naver.com>
2025-06-20 12:39:58 +0900

    PostRepository 작성

---
+++ src/main/java/com/board/domain/PostRepository.java
@@ -0,0 +1,36 @@
+package com.board.domain;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+@Transactional
+public class PostRepository {
+
+    private final EntityManager em;
+
+    public void save(Post post) {
+        em.persist(post);
+    }
+
+    public Post findById(Long id) {
+        return em.find(Post.class, id);
+    }
+
+    // todo 임시
+    public List<Post> findByTitle(String title) {
+        return em.createQuery("select p from Post p where p.title like :title", Post.class)
+                .setParameter("title", title)
+                .getResultList();
+    }
+
+    public List<Post> findAll() {
+        return em.createQuery("select p from Post p", Post.class)
+                .getResultList();
+    }
+}
