5e64b9b38e2b18657553a7dbc28c6fb2b8060706
Yechan Moon <moonyechan97@naver.com>
2025-06-20 18:01:48 +0900

    수정을 위한 PostUpdateDto 객체 추가

---
+++ src/main/java/com/board/dto/PostUpdateDto.java
@@ -0,0 +1,22 @@
+package com.board.dto;
+
+import com.board.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class PostUpdateDto {
+    private Long id;
+    private String title;
+    private String contents;
+
+    public static PostUpdateDto of(Post post) {
+        return new PostUpdateDto(post.getId(), post.getTitle(), post.getContents());
+    }
+
+    public static PostUpdateDto of(Long postId, PostWriteDto writeDto) {
+        return new PostUpdateDto(postId, writeDto.getTitle(), writeDto.getContents());
+    }
+
+}
