62cbb27cf3f8eb7f25f6246b9533ce556f1679f4
Yechan Moon <moonyechan97@naver.com>
2025-06-20 17:58:30 +0900

    저장 리다이렉션 주소 변경, 수정, 삭제, 복구, 완전삭제 요청받는 메서드 추가

--- src/main/java/com/board/web/PostController.java
+++ src/main/java/com/board/web/PostController.java
@@ -2,6 +2,9 @@ package com.board.web;
 
 import com.board.domain.Post;
 import com.board.domain.PostRepository;
+import com.board.domain.PostService;
+import com.board.dto.PostUpdateDto;
+import com.board.dto.PostWriteDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +20,7 @@ import java.util.List;
 public class PostController {
 
     private final PostRepository postRepository;
+    private final PostService postService;
 
     @GetMapping("/posts")
     public String showAllPosts(Model model) {
@@ -40,7 +44,52 @@ public class PostController {
 
     @PostMapping("/posts/write")
     public String write(@ModelAttribute PostWriteDto postWriteDto) {
-        postRepository.save(postWriteDto.convertToPostEntity());
+        Long savedId = postRepository.save(postWriteDto.convertToPostEntity());
+        return "redirect:/posts/" + savedId;
+    }
+
+    @GetMapping("/posts/{postId}/edit")
+    public String editForm(@PathVariable Long postId, Model model) {
+        Post post = postRepository.findById(postId);
+        model.addAttribute("form", PostUpdateDto.of(post));
+        return "posts/editForm";
+    }
+
+    @PostMapping("/posts/{postId}/edit")
+    public String edit(@ModelAttribute PostWriteDto writeDto, @PathVariable Long postId) {
+        postService.edit(PostUpdateDto.of(postId, writeDto));
+        return "redirect:/posts/" + postId;
+    }
+
+    @PostMapping("/posts/{postId}/delete")
+    public String delete(@PathVariable Long postId) {
+        postService.delete(postId);
         return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/deleted")
+    public String deletedList(Model model) {
+        List<Post> deletedPosts = postRepository.findDeletedPosts();
+        model.addAttribute("posts", deletedPosts);
+        return "posts/deletedList";
+    }
+
+    @GetMapping("/posts/deleted/{postId}")
+    public String deletedPost(@PathVariable Long postId, Model model) {
+        Post deletedPost = postRepository.findById(postId);
+        model.addAttribute("post", deletedPost);
+        return "posts/deletedPost";
+    }
+
+    @PostMapping("/posts/deleted/{postId}/restore")
+    public String restore(@PathVariable Long postId) {
+        postService.restore(postId);
+        return "redirect:/posts/" + postId;
+    }
+
+    @PostMapping("/posts/deleted/{postId}/full-delete")
+    public String fullDelete(@PathVariable Long postId) {
+        postService.fullDelete(postId);
+        return "redirect:/posts/deleted";
     }
 }
